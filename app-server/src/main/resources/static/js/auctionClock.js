dcb34f8b9608ea8a51ce5b160b330f889ff57e0c
same-woo <pardongwoo@naver.com>
2023-09-22 09:38:05 +0900

    f2

--- app-server/src/main/resources/static/js/auctionClock.js
+++ app-server/src/main/resources/static/js/auctionClock.js
@@ -1,35 +1,32 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var tid = 0;
-    var p = document.getElementById("RemainPeriod");
-    if (p) {
-        var period = parseInt(p.textContent);
+// HTML 요소 찾기
+var periodElement = document.getElementById('userNo');
 
-        function auctionClock() {
-            period -= 1;
-            if (period < 0) {
-                if (tid) clearTimeout(tid);
-                window.setTimeout(function () {
-                    location.reload();
-                }, 1000);
-                return;
-            }
+// 가져온 정보에서 필요한 값을 추출
+var period = parseInt(periodElement.textContent);
 
-            var strTime = "";
-            var day = parseInt(period / (24 * 60 * 60));
-            var hour = parseInt((period / 60 / 60) % 24);
-            var minute = parseInt((period / 60) % 60);
-            var second = parseInt(period % 60);
+var tid = 0;
 
-            if (day > 0) {
-                strTime = day + "일 ";
-            }
-            strTime = strTime + hour + "시간 " + minute + "분 " + second + "초";
-
-            if (p) p.innerHTML = strTime;
-            if (tid) clearTimeout(tid);
-            tid = window.setTimeout(auctionClock, 1000);
-        }
-
-        if (period > 0) auctionClock();
+function auctionClock() {
+    period -= 1;
+    if (period < 0) {
+        if (tid) clearTimeout(tid);
+        window.setTimeout("location.reload();", 1000);
+        return;
     }
-});
+
+    strTime = '';
+    var day = parseInt(period / (24 * 60 * 60));
+    var hour = parseInt((period / 60 / 60) % 24);
+    var minute = parseInt((period / 60) % 60);
+    var second = parseInt(period % 60);
+
+    if (day > 0) strTime = day + '일 '
+    strTime = strTime + hour + '시간 ' + minute + '분 ' + second + '초';
+
+    var p = document.getElementById("RemainPeriod");
+    if (p) p.innerHTML = strTime;
+    if (tid) clearTimeout(tid);
+    tid = window.setTimeout("auctionClock()", 1000);
+}
+
+if (period > 0) auctionClock();
--- app-server/src/main/resources/static/js/purchaseScript.js
+++ app-server/src/main/resources/static/js/purchaseScript.js
@@ -1,64 +1,64 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var purchaseButton = document.getElementById('purchaseButton');
+document.getElementById('purchaseButton').addEventListener('click', () => {
+    var purchasePrice = parseInt(
+        document.getElementById('endPrice').textContent.replace(/\D/g, '')
+    ); // 즉시구매 가격
+    var userPoints = parseInt(
+        document.getElementById('userPoints').textContent.replace(/\D/g, '')
+    ); // 보유 포인트를 가져옴
 
-    if (purchaseButton) {
-        purchaseButton.addEventListener('click', function () {
-            var articleNo = parseInt(
-                document.getElementById('articleNo').textContent.replace(/\D/g, '')
-            );
-            var currentPage = parseInt(
-                document.getElementById('userPoints').textContent.replace(/\D/g, '')
-            );
-            var purchasePrice = parseInt(
-                document.getElementById('endPrice').textContent.replace(/\D/g, '')
-            );
+    // 알림창으로 즉시구매 정보 표시
+    var confirmation = confirm(
+        '즉시구매가격: ' + purchasePrice + '\n현재 포인트: ' + userPoints + '\n\n정말 즉시구매 하시겠습니까?'
+    );
 
-            var userPoints = parseInt(
-                document.getElementById('userPoints').textContent.replace(/\D/g, '')
-            );
+    // HTML 요소 찾기
+    var userNoElement = document.getElementById('userNo');
+    var articleNoElement = document.getElementById('articleNo');
+    var currentPageElement = document.getElementById('currentPage');
+    var pathElement = document.getElementById('path');
 
-            var userNoElement = document.getElementById('userNo');
-            var articleNoElement = document.getElementById('articleNo');
 
-            var userNo = parseInt(userNoElement.textContent);
-            var articleNo = parseInt(articleNoElement.textContent);
+// 가져온 정보에서 필요한 값을 추출
+    var userNo = parseInt(userNoElement.textContent);
+    var articleNo = parseInt(articleNoElement.textContent);
+    var currentPage = parseInt(currentPageElement.textContent);
+    var path = parseInt(pathElement.textContent);
 
-            var confirmation = confirm(
-                '즉시구매가격: ' + purchasePrice + '\n현재 포인트: ' + userPoints + '\n\n정말 즉시구매 하시겠습니까?'
-            );
 
-            function redirectToArticleDetail(articleNo) {
-                var redirectURL = '/article/detail?articleNo=' + articleNo + '&path=0&currentPage=' + currentPage;
+    // redirectToArticleDetail 함수 정의
+    function redirectToArticleDetail() {
+        var articleDetailURL = '/article/detail?articleNo=' + articleNo + '&path=' + path + '&currentPage=' + currentPage;
+        window.location.href = articleDetailURL;
+    }
 
-            }
 
-            if (confirmation) {
-                if (userPoints < purchasePrice) {
-                    return alert("포인트가 부족합니다.");
+    if (confirmation) {
+        if (userPoints < purchasePrice) {
+            return alert("포인트가 부족합니다.");
+        }
+
+        // AJAX 요청
+        $.ajax({
+            url: '/points/purchasePoint', // 즉시구매 API 엔드포인트 URL
+            method: 'POST',
+            dataType: "json",
+            contentType: "application/json; charset=utf-8",
+            async: false, // 동기적으로 서버응답을 처리.
+            data: JSON.stringify({
+                userNo: userNo, // 사용자 번호
+                articleNo: articleNo, // 게시글 번호
+                bidAmount: purchasePrice, // 즉시구매 가격
+            }),
+            success: (response) => {
+                console.log(response);
+                if (response === true) {
+                    alert("즉시구매가 완료되었습니다.");
+                    redirectToArticleDetail();
+
+
+                } else {
+                    alert("즉시구매가 실패했습니다."); // 실패 처리
                 }
-
-                // AJAX 요청
-                $.ajax({
-                    url: '/points/purchasePoint', // 즉시구매 API 엔드포인트 URL
-                    method: 'POST',
-                    dataType: "json",
-                    contentType: "application/json; charset=utf-8",
-                    async: false,
-                    data: JSON.stringify({
-                        userNo: userNo,
-                        articleNo: articleNo,
-                        bidAmount: purchasePrice,
-                    }),
-                    success: function (response) {
-                        console.log(response);
-                        if (response === true) {
-                            alert("즉시구매가 완료되었습니다.");
-                            window.location.href = redirectURL;
-                        } else {
-                            alert("즉시구매가 실패했습니다.");
-                        }
-                    }
-                });
             }
         });
     }
